020d2a96ceb887ecb167d3d3762353ad3fc73f62
kalz2q <katsu95i@gmail.com>
2019-04-19 15:34:57 +0900

    m

--- src/Main.elm
+++ src/Main.elm
@@ -1,41 +1,17 @@
-import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+module Main exposing (main)
+
+import Html exposing (Html, a, text, div, h1, ul, li)
+import Html.Attributes exposing (href)
 
 
+main : Html msg
 main =
-  Browser.sandbox { init = init, update = update, view = view }
-
-
--- MODEL
-
-type alias Model = Int
-
-init : Model
-init =
-  0
-
-
--- UPDATE
-
-type Msg = Increment | Decrement
-
-update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    Increment ->
-      model + 1
-
-    Decrement ->
-      model - 1
-
-
--- VIEW
-
-view : Model -> Html Msg
-view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ h1 [] [ text "Useful Links" ]
+        , ul []
+            , li [] [ a [ href "src/Counter.html" ] [ text "Counter" ] ]
+            [ li [] [ a [ href "https://elm-lang.org" ] [ text "Homepage" ] ]
+            , li [] [ a [ href "https://package.elm-lang.org" ] [ text "Packages" ] ]
+            , li [] [ a [ href "https://ellie-app.com" ] [ text "Playground" ] ]
+            ]
+        ]
