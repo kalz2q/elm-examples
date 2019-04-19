cd4fcf92b0ec5ca85476a1ce3821e63aa8fd70c4
kalz2q <katsu95i@gmail.com>
2019-04-19 20:51:28 +0900

    m

--- src/Main.elm
+++ src/Main.elm
@@ -7,11 +7,11 @@ import Html.Attributes exposing (..)
 main : Html msg
 main =
     div []
-        [ h1 [style "font-family" "serif"] [ text "Links to Samples" ]
+        [ h1 [ style "font-family" "serif" ] [ text "Links to Samples" ]
         , p [] []
         , ul []
-            [ li [] [ a [ href "src/Counter.html" ] [ text "Counter" ] ]
-            , li [] [ a [ href "src/Form.html"] [ text "Form"]]
+            [ li [] [ a [ href "src/Form.html" ] [ text "Counter" ] ]
+            , li [] [ a [ href "src/Counter.html" ] [ text "Form" ] ]
             ]
         , p [ style "font-size" "30px", style "font-family" "Noto Serif CJK JP" ] [ text "コピペしたり試行錯誤してElmから作ったもののリストです。ソースは以下をご覧ください。" ]
         , a [ href "https://github.com/kalz2q/elm-projects" ] [ text "https://github.com/kalz2q/elm-projects" ]
