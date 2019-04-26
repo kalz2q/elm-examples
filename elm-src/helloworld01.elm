2cafe600859333aac2f6af834922103b86d6dfe8
kalz2q <katsu95i@gmail.com>
2019-04-26 09:59:33 +0900

    m

--- elm-src/helloworld01.elm
+++ elm-src/helloworld01.elm
@@ -1,16 +1,8 @@
--- 
-module Main exposing (main)
-
--- We're importing the Html module the text value available in our file, so we
--- can just reference it if we want.
+module SimpleHelloWorldElm exposing (main)
 
 import Html exposing (text)
 
-
-
--- The main value manages what gets displayed on the page. If we set the main
--- value to (text "Hello, World!"), then a text node with the string "Hello, World!"
--- will display on the page.
+-- 一番簡単なElmによるハローワールドです。
 
 
 main =
