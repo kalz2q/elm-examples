ac3cc0651d43034e3797b9f159c79fe1a50eec5a
kalz2q <katsu95i@gmail.com>
2019-05-13 14:05:00 +0900

    m

---
+++ src/helloworldelmui.elm
@@ -0,0 +1,16 @@
+module SimpleHelloWorldElm exposing (main)
+
+
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events exposing (..)
+import Element.Font as Font
+import Element.Input as Input
+import Html exposing (Html)
+
+
+main =
+  layout [] 
+    (el [centerX, centerY] (text "Cats, the Gathering"))
+  
