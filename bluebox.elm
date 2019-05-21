ad86d7891c2b85409b5dbaa6d50b8aeb55d62bd3
kalz2q <katsu95i@gmail.com>
2019-05-21 20:17:17 +0900

    m

---
+++ bluebox.elm
@@ -0,0 +1,18 @@
+module BlueBox exposing (main)
+
+-- import Browser
+import Element exposing (..)
+import Element.Background as Background
+-- import Element.Border as Border
+-- import Element.Events exposing (..)
+-- import Element.Font as Font
+-- import Element.Input as Input
+import Html exposing (Html)
+-- import Task
+-- import Time
+
+
+main =
+  layout [] <|
+    column [] <|
+      (text "hello")
