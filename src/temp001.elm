724a5fa86e231c3451b1ddee60d90c7bbe5c78c3
kalz2q <katsu95i@gmail.com>
2019-06-14 17:23:04 +0900

    m

--- src/temp001.elm
+++ src/temp001.elm
@@ -1,20 +1,33 @@
-import Html
-import Browser
 
-main : Program () model msg
+
+import Browser
+import Html
+
+
 main =
-  Browser.sandbox 
-    { init = init
-    , update =  update
-    , view = view
-    }
- 
- 
-type alias Model = String
-  
-  nit = ""
-  
-  view =
-    dov [] [
-       Html.text "hello, world!"
-       ]
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
+
+
+
+type alias Model =
+    String
+
+init : Model 
+init = ""
+
+type alias Msg  =
+  String
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        _ ->
+            " "
+
+-- view : Model -> Html.Html Msg
+view =
+    Html.div [] [Html.text "Hello, world!!"]
