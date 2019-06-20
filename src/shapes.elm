e59b1f8a27ff7806ededccc27db1e26fb5d26ea8
kalz2q <katsu95i@gmail.com>
2019-06-20 13:41:57 +0900

    m

--- src/shapes.elm
+++ src/shapes.elm
@@ -1,4 +1,4 @@
-module Shapes exposing (main)
+-- module Shapes exposing (main)
 
 import Html
 import Svg
@@ -21,24 +21,55 @@ main =
                 , SA.strokeWidth "7"
                 ]
                 []
+            , Svg.circle
+                [ SA.cx "25"
+                , SA.cy "75"
+                , SA.r "20"
+                , SA.stroke "red"
+                , SA.fill "skyblue"
+                , SA.strokeWidth "5"
+                ]
+                []
+            , Svg.ellipse
+                [ SA.cx "75"
+                , SA.cy "75"
+                , SA.rx "20"
+                , SA.ry "5"
+                , SA.stroke "red"
+                , SA.fill "skyblue"
+                , SA.strokeWidth "5"
+                ]
+                []
+            , Svg.line
+                [ SA.x1 "10"
+                , SA.x2 "59"
+                , SA.y1 "110"
+                , SA.y2 "150"
+                , SA.stroke "orange"
+                , SA.strokeWidth "5"
+                ]
+                []
+            , Svg.polyline
+                [ SA.points "60 110 65 120 70 115 75 130 80 125 85 140 90 135 95 150 100 145"
+                , SA.stroke "orange"
+                , SA.fill "beige"
+                , SA.strokeWidth "5"
+                ]
+                []
+            , Svg.polygon
+                [ SA.points "50 160 55 180 70 180 60 190 65 205 50 195 35 205 40 190 30 180 45 180"
+                , SA.stroke "green"
+                , SA.fill "beige"
+                , SA.strokeWidth "5"
+                ]
+                []
+            , Svg.path
+                [ SA.d "M20,230 Q40,205 50,230 T90,230"
+                , SA.stroke "blue"
+                , SA.fill "none"
+                , SA.strokeWidth "5"
+                ]
+                []
             ]
-            , Svg.circle [][]
-            
-
-            
         ]
 
-
-
--- <svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
---   <rect x="10" y="10" width="30" height="30" stroke="black" fill="transparent" stroke-width="5"/>
---   <rect x="60" y="10" rx="10" ry="10" width="30" height="30" stroke="black" fill="transparent" stroke-width="5"/>
---   <circle cx="25" cy="75" r="20" stroke="red" fill="transparent" stroke-width="5"/>
---   <ellipse cx="75" cy="75" rx="20" ry="5" stroke="red" fill="transparent" stroke-width="5"/>
---   <line x1="10" x2="50" y1="110" y2="150" stroke="orange" stroke-width="5"/>
---   <polyline points="60 110 65 120 70 115 75 130 80 125 85 140 90 135 95 150 100 145"
---       stroke="orange" fill="transparent" stroke-width="5"/>
---   <polygon points="50 160 55 180 70 180 60 190 65 205 50 195 35 205 40 190 30 180 45 180"
---       stroke="green" fill="transparent" stroke-width="5"/>
---   <path d="M20,230 Q40,205 50,230 T90,230" fill="none" stroke="blue" stroke-width="5"/>
--- </svg>
