f7507033ecae41713fc60c26e42884af367e8b7a
kalz2q <katsu95i@gmail.com>
2019-07-23 15:08:21 +0900

    m

---
+++ src/picshare001.elm
@@ -0,0 +1,13 @@
+module Picshare001 exposing (main)
+
+import Html
+import Html.Attributes as HA
+
+main : Html.Html msg
+main =
+    div [
+          background-color: #fff;
+  padding-bottom: 10px;
+  padding-top: 10px;
+  text-align: center;
+    ] [ text "Picshare" ]
