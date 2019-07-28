d2660e5bc1c6fa0cf5fe202b048d021ecbca4a8c
kalz2q <katsu95i@gmail.com>
2019-07-28 11:58:51 +0900

    m

---
+++ src/picshare007.elm
@@ -0,0 +1,152 @@
+module Picshare006 exposing (main)
+
+-- p.61 refactoring
+
+import Browser
+import Html
+import Html.Attributes as HA
+import Html.Events as HE
+import Svg
+import Svg.Attributes as SA
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
+
+
+type alias Model =
+    { url : String, caption : String, liked : Bool }
+
+
+initialModel : Model
+initialModel =
+    { url = "https://drive.google.com/uc?id=1NyeKCX2Hh0iioPYQs7JsJ8e_okLC4L5Y"
+    , caption = "Santa Clause"
+    , liked = False
+    }
+
+
+type Msg
+    = Unlike
+    | Like
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Like ->
+            { model | liked = True }
+
+        Unlike ->
+            { model | liked = False }
+
+
+viewDetailedPhoto : Model -> Html.Html Msg
+viewDetailedPhoto model =
+    let
+        buttonClass =
+            if model.liked then
+                "fa-heart"
+
+            else
+                "fa-heart-o"
+
+        msg =
+            if model.liked then
+                Unlike
+
+            else
+                Like
+    in
+    Html.div
+        [ HA.class "detailed-photo"
+        , HA.style "box-shadow" "0 0 10px #555"
+        , HA.style "background" "yellow"
+        ]
+        [ Html.img
+            [ HA.src model.url
+            , HA.style "width" "400px"
+            , HA.style "margin-top" "10px"
+            ]
+            []
+        , Html.div
+            [ HA.class "photo-info"
+            , HA.style "padding-bottom" "10px"
+            ]
+            [ Html.div [ HA.class "like-button" ]
+                [ Html.span
+                    [ HA.class "fa fa-2x"
+                    , HA.class buttonClass
+                    , HE.onClick msg
+                    ]
+                    [ if model.liked then
+                        pinkheart
+
+                      else
+                        blackheart
+                    ]
+                ]
+            , Html.h2
+                [ HA.class "caption"
+                , HA.style "font-size" "30px"
+                , HA.style "font-weight" "lighter"
+                , HA.style "font-style" "italic"
+                , HA.style "margin" "0 0 10px 0"
+                ]
+                [ Html.text model.caption ]
+            ]
+        ]
+
+
+view : { url : String, caption : String, liked : Bool } -> Html.Html Msg
+view model =
+    Html.div []
+        [ Html.div
+            [ HA.class "header"
+            , HA.style "background-color" "#aaa"
+            , HA.style "padding-bottom" "10px"
+            , HA.style "padding-top" "10px"
+            , HA.style "text-align" "center"
+            ]
+            [ Html.h1 [] [ Html.text "Picshare" ] ]
+        , Html.div
+            [ HA.class "content-flow"
+            , HA.style "margin" "auto"
+            , HA.style "width" "400px"
+            ]
+            [ viewDetailedPhoto model
+            ]
+        ]
+
+
+blackheart =
+    Svg.svg
+        [ SA.width "28"
+        , SA.height "28"
+        , SA.viewBox "0 0 28 28"
+        ]
+        [ Svg.path
+            [ SA.d "M14 26c-0.25 0-0.5-0.094-0.688-0.281l-9.75-9.406c-0.125-0.109-3.563-3.25-3.563-7 0-4.578 2.797-7.313 7.469-7.313 2.734 0 5.297 2.156 6.531 3.375 1.234-1.219 3.797-3.375 6.531-3.375 4.672 0 7.469 2.734 7.469 7.313 0 3.75-3.437 6.891-3.578 7.031l-9.734 9.375c-0.187 0.187-0.438 0.281-0.688 0.281z"
+            , SA.fill "black"
+            ]
+            []
+        ]
+
+
+pinkheart =
+    Svg.svg
+        [ SA.width "28"
+        , SA.height "28"
+        , SA.viewBox "0 0 28 28"
+        ]
+        [ Svg.path
+            [ SA.d "M14 26c-0.25 0-0.5-0.094-0.688-0.281l-9.75-9.406c-0.125-0.109-3.563-3.25-3.563-7 0-4.578 2.797-7.313 7.469-7.313 2.734 0 5.297 2.156 6.531 3.375 1.234-1.219 3.797-3.375 6.531-3.375 4.672 0 7.469 2.734 7.469 7.313 0 3.75-3.437 6.891-3.578 7.031l-9.734 9.375c-0.187 0.187-0.438 0.281-0.688 0.281z"
+            , SA.fill "pink"
+            ]
+            []
+        ]
