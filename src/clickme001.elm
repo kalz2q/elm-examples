c24fdd4f9033af3dc601efa4093a6c8514d7c1f2
kalz2q <katsu95i@gmail.com>
2019-09-24 12:20:19 +0900

    m

--- src/clickme001.elm
+++ src/clickme001.elm
@@ -1,4 +1,4 @@
-module ClickMe001 exposing (..)
+module ClickMe001 exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
 import Html exposing (..)
@@ -18,51 +18,106 @@ main =
 type alias Model =
     { string : String
     , background : String
-    , borderRadius : String }
+    , borderRadius : String
+    }
 
 
 init : Model
 init =
     { string = "Click Me!"
-    , background = "pink" 
-    , borderRadius = "0%"}
+    , background = "pink"
+    , borderRadius = "0%"
+    }
 
 
 type Msg
-    = Click
+    = Click1 | Click2
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Click ->
+        Click1 ->
             if model.background == "pink" then
-                { model | background = "skyblue"
-                , borderRadius = "50%" }
+                { model
+                    | background = "skyblue"
+                    , borderRadius = "50%"
+                }
 
             else
-                { model | background = "pink"
-                , borderRadius = "0%" }
+                { model
+                    | background = "pink"
+                    , borderRadius = "0%"
+                }
+
+        Click2 ->
+            if model.background == "pink" then
+                { model
+                    | background = "skyblue"
+                    , borderRadius = "50%"
+                }
+
+            else
+                { model
+                    | background = "pink"
+                    , borderRadius = "0%"
+                }
 
 
 view : Model -> Html.Html Msg
 view model =
     div
-        [ HA.style "class" "box"
-        , HA.style "width" "200px"
-        , HA.style "height" "200px"
-        , HA.style "margin" "60px auto"
-        , HA.style "position" "relative"
-        , HA.style "cursor" "pointer"
-        , HA.style "background" model.background
-        , HA.style "borderRadius" model.borderRadius
-        , HE.onClick Click
+        [ HA.style "display" "flex"
+        , HA.style "flex-wrap" "wrap"
+                    , HA.style "width" "600px"
+                                , HA.style "margin" "auto"
+        
         ]
         [ div
-            [ HA.style "position" "absolute"
-            , HA.style "top" "50%"
-            , HA.style "left" "50%"
-            , HA.style "transform" "translate(-50%, -50%)"
+            [ HA.style "class" "box"
+            , HA.style "width" "200px"
+            , HA.style "height" "200px"
+            , HA.style "margin" "60px auto"
+            , HA.style "position" "relative"
+            , HA.style "cursor" "pointer"
+            , HA.style "background" model.background
+            , HA.style "borderRadius" model.borderRadius
+
+            -- , HA.style "transform" "rotate(180deg)"
+            , HA.style "transition" "0.8s"
+            , HE.onClick Click1
             ]
-            [ text model.string ]
+            [ div
+                [ HA.style "position" "absolute"
+                , HA.style "top" "50%"
+                , HA.style "left" "50%"
+                , HA.style "transition" "0.8s"
+                , HA.style "transform" "translate(-50%, -50%)"
+                ]
+                [ text model.string ]
+            ]
+            ,
+            div
+            [ HA.style "class" "box"
+            , HA.style "width" "200px"
+            , HA.style "height" "200px"
+            , HA.style "margin" "60px auto"
+            , HA.style "position" "relative"
+            , HA.style "cursor" "pointer"
+            , HA.style "background" model.background
+            , HA.style "borderRadius" model.borderRadius
+
+            -- , HA.style "transform" "rotate(180deg)"
+            , HA.style "transition" "0.8s"
+            , HE.onClick Click2
+            ]
+            [ div
+                [ HA.style "position" "absolute"
+                , HA.style "top" "50%"
+                , HA.style "left" "50%"
+                , HA.style "transition" "0.8s"
+                , HA.style "transform" "translate(-50%, -50%)"
+                ]
+                [ text model.string ]
+            ]
         ]
