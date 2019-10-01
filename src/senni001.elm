6c2fd13c4c512e9f6844e8952e5bd8db8c110ca9
kalz2q <katsu95i@gmail.com>
2019-10-01 11:26:46 +0900

    m

---
+++ src/senni001.elm
@@ -0,0 +1,11 @@
+module Senni001 exposing (main)
+
+import Html exposing (..)
+import Html.Attributes as HA
+import Html.Events as HE
+
+main =
+    div []
+        [ a [ HA.href "https://ellie-app.com/5PbTQXnR23wa1" ] [ text "遷移" ]
+        -- , button [HE.onClick "location.href='./index.html'"][text "次"]
+        ]
