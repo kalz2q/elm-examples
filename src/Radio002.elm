4b81e52c38adf02c885c8512abc51adb3fa761f4
kalz2q <katsu95i@gmail.com>
2019-11-06 09:16:43 +0900

    m

---
+++ src/Radio002.elm
@@ -0,0 +1,81 @@
+module Radio001 exposing (main)
+
+-- reading radio_buttons.md
+
+import Browser
+import Html exposing (..)
+import Html.Attributes as HA
+import Html.Events as HE
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { fontSize : FontSize
+    , content : String
+    }
+
+
+type FontSize
+    = Small
+    | Medium
+    | Large
+
+
+init : Model
+init =
+    Model Small "This is a sample content."
+
+
+type Msg
+    = SwitchTo FontSize
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ fieldset []
+            [ radio (SwitchTo Small) "Small"
+            , radio (SwitchTo Medium) "Medium"
+            , radio (SwitchTo Large) "Large"
+            ]
+        , section [] [ text model.content ]
+        ]
+
+
+radio : msg -> String -> Html msg
+radio msg name =
+    label []
+        [ input [ HA.type_ "radio", HE.onClick msg ] []
+        , text name
+        ]
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        SwitchTo newFontSize ->
+            { model | fontSize = newFontSize }
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
