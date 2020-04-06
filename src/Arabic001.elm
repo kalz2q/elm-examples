93677c366c1878e1d94df7ed0fbf3b57ef2e4951
kalz2q <katsu95i@gmail.com>
2020-04-06 13:47:32 +0900

    m

--- src/Arabic001.elm
+++ src/Arabic001.elm
@@ -4,6 +4,7 @@ import Browser
 import Html exposing (..)
 import Html.Attributes as HA
 import Html.Events as HE
+import Random
 
 
 main : Program () Model Msg
@@ -20,19 +21,32 @@ type alias Model =
     { latin : String
     , arabic : String
     , meaning : String
+    , showanswer : Bool
+    , list : List Arabicdata
+    }
+
+
+type alias Arabicdata =
+    { latin : String
+    , arabic : String
+    , meaning : String
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model "" "" ""
-    , Cmd.none
+    ( Model "" "" "" False dict
+    , Random.generate NewList (shuffle dict)
     )
 
 
 type Msg
     = Delete
-    | KeepNext
+    | Next
+    | NewRandom Int
+    | Shuffle
+    | NewList (List Arabicdata)
+    | ShowAnswer Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -43,10 +57,54 @@ update msg model =
             , Cmd.none
             )
 
-        KeepNext ->
+        Next ->
             ( model
             , Cmd.none
             )
+
+        NewRandom n ->
+            case List.drop n dict of
+                x :: _ ->
+                    ( { model
+                        | latin = x.latin
+                        , arabic = x.arabic
+                        , meaning = x.meaning
+                      }
+                    , Cmd.none
+                    )
+
+                [] ->
+                    ( model
+                    , Cmd.none
+                    )
+
+        Shuffle ->
+            ( model
+            , Random.generate NewList
+                (shuffle dict)
+            )
+
+        NewList newList ->
+            ( { model | list = newList }
+            , Cmd.none
+            )
+
+        ShowAnswer index ->
+            case List.drop index model.list of
+                x :: _ ->
+                    ( { model
+                        | latin = x.latin
+                        , arabic = x.arabic
+                        , meaning = x.meaning
+                        , showanswer = True
+                      }
+                    , Cmd.none
+                    )
+
+                [] ->
+                    ( model
+                    , Cmd.none
+                    )
 
 
 subscriptions : Model -> Sub Msg
@@ -63,6 +121,28 @@ view model =
         , button [] [ text "Delete" ]
         , button [] [ text "Next" ]
         ]
+
+
+shuffle : List a -> Random.Generator (List a)
+shuffle list =
+    let
+        randomNumbers : Random.Generator (List Int)
+        randomNumbers =
+            Random.list (List.length list) <| Random.int Random.minInt Random.maxInt
+
+        zipWithList : List Int -> List ( a, Int )
+        zipWithList intList =
+            List.map2 Tuple.pair list intList
+
+        listWithRandomNumbers : Random.Generator (List ( a, Int ))
+        listWithRandomNumbers =
+            Random.map zipWithList randomNumbers
+
+        sortedGenerator : Random.Generator (List ( a, Int ))
+        sortedGenerator =
+            Random.map (List.sortBy Tuple.second) listWithRandomNumbers
+    in
+    Random.map (List.map Tuple.first) sortedGenerator
 
 
 dict =
@@ -90,9 +170,85 @@ dict =
       , arabic = "أَنا بِخَيْر"
       , meaning = "I'm fine"
       }
+    , { latin = "kabir"
+      , arabic = "كَبير"
+      , meaning = "large, big"
+      }
+    , { latin = "bayt"
+      , arabic = "بَيْت"
+      , meaning = "a house"
+      }
+    , { latin = "laysa"
+      , arabic = "لِيْسَ"
+      , meaning = "do not"
+      }
+    , { latin = "3and"
+      , arabic = "عَنْد"
+      , meaning = "have"
+      }
+    , { latin = "siith"
+      , arabic = "سيث"
+      , meaning = "Seth(male name)"
+      }
+    , { latin = "baluuzah"
+      , arabic = "بَلوزة"
+      , meaning = "blouse"
+      }
+    , { latin = "tii shiirt"
+      , arabic = "تي شيرْت"
+      , meaning = "T-shirt"
+      }
+    , { latin = "mi3Taf"
+      , arabic = "مِعْطَف"
+      , meaning = "a coat"
+      }
+    , { latin = "riim"
+      , arabic = "ريم"
+      , meaning = "Reem(name)"
+      }
+    , { latin = "tannuurah"
+      , arabic = "تَنّورة"
+      , meaning = "a skirt"
+      }
+    , { latin = "jadiid"
+      , arabic = "جَديد"
+      , meaning = "new"
+      }
+    , { latin = "wishshaaH"
+      , arabic = "وِشَاح"
+      , meaning = "a scarf"
+      }
+    , { latin = "judii"
+      , arabic = "جودي"
+      , meaning = "Judy(name)"
+      }
+    , { latin = "jamiil"
+      , arabic = "جَميل"
+      , meaning = "good, nice"
+      }
+    , { latin = "kalb"
+      , arabic = "كَلْب"
+      , meaning = "a dog"
+      }
+    , { latin = "2azraq"
+      , arabic = "أَزْرَق"
+      , meaning = "blue"
+      }
+    , { latin = "2abyad"
+      , arabic = "أَبْيَض "
+      , meaning = "white"
+      }
+    , { latin = "qubb3ah"
+      , arabic = "قُبَّعة"
+      , meaning = "a hat"
+      }
+    , { latin = "bunniyy"
+      , arabic = "بُنِّيّ"
+      , meaning = "brown"
+      }
 
-    --, { latin  = "2alaykum"
-    --   , arabic = "عَلَيْكُمْ"
-    --   , meaning = "on you"
-    -- }
+    -- , { latin  = ""
+    --   , arabic = ""
+    --   , meaning = ""
+    --   }
     ]
