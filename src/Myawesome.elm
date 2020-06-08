0df780f383b35f276d2e81c3bc50b230608b327f
kalz2q <katsu95i@gmail.com>
2020-06-08 09:25:08 +0900

    m

--- src/Myawesome.elm
+++ src/Myawesome.elm
@@ -1,8 +1,13 @@
 module Myawesome exposing (main)
 
+-- this is a kind of translation from dotinstall myawesome application
+-- in order to change the application to smart phone format, add following line in the html version
+-- <meta name="viewport" content="width=device-width, initial-scale=1">
+-- html version is like this elm make src/Myawesome.elm --output=Myawesome.html
+
 import Browser
 import Html exposing (..)
-import Html.Attributes as HA
+import Html.Attributes as HA exposing (name)
 import Html.Events as HE
 import Random
 
@@ -44,18 +49,38 @@ subscriptions model =
     Sub.none
 
 
+header : List (Html msg) -> Html msg
+header children =
+    div [ HA.style "font-size" "30pt" ] children
+
+
+button : List (Attribute msg) -> List (Html msg) -> Html msg
+button attributes children =
+    div ([ HA.style "font-size" "40pt" ] ++ attributes) children
+
+
 view : Model -> Html Msg
 view model =
-    div []
-        [ header []
-            [ h1 [] [ text "すごいアプリ" ]
-            , p [] [ text "ウルトラすごいアプリです" ]
-            , a [] [ text "ダウンロード" ]
+    header
+        [ div
+            [ HA.style "background" "#a59a55"
+            , HA.style "color" "#fff"
+            , HA.style "text-align" "center"
+            , HA.style "padding-top" "16px"
+            ]
+            [ div [] [ text "すごいアプリ" ]
+            , div [] [ text "ウルトラすごいアプリです" ]
+            , button
+                [ HA.style "font-weight" "bold"
+                ]
+                [ text "ダウンロード" ]
+            , div [] [ text "لَيْسَ عِنْدِك وِشاح" ]
             ]
         , section [] []
         , footer []
             [ text "Convert latin to arabic"
-            , p [] [ text "SabaaH" ]
+            , div [] [ text "SabaaH" ]
+            , div [] [ text "next test" ]
             , button [] [ text "Show Answer" ]
             , button [] [ text "Delete" ]
             , button [] [ text "Next" ]
